0ee03608942a271a073ffd2c0a9db0d2fd8c103d
Rahul Sharma <rah208sh@gmail.com>
2022-05-31 01:26:52 -0400

    Constant

---
+++ Constants/Constants.js
@@ -0,0 +1,13 @@
+//VAriables values can be updated but constant is fixed
+
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate)
+
+//in above code the value is updated as in variables it can reassigned.
+
+
+// Constant
+
+const interestRate1 = 10;
+console.log(interestRate1)
