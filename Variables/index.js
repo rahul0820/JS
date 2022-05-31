4e5e9e594cda912afa02bc26a5795612585de59a
Rahul Sharma <rah208sh@gmail.com>
2022-05-31 00:28:30 -0400

    JS basics

---
+++ Variables/index.js
@@ -0,0 +1,9 @@
+//we use variable to store data temporarily. 
+//before ES6 - generally used to VAR to declare variable.
+//after ES6 it change to LET to declare variable.
+
+let name = "Rahul";
+console.log(name);
+
+//rules for naming the variables
+//cannot be a reserved Keyword
