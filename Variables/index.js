55e089e53f073dfe3d0f5d3d5580f3e216c82629
Rahul Sharma <rah208sh@gmail.com>
2022-05-31 01:19:56 -0400

    Variables

--- Variables/index.js
+++ Variables/index.js
@@ -1,9 +1,26 @@
 //we use variable to store data temporarily. 
-//before ES6 - generally used to VAR to declare variable.
-//after ES6 it change to LET to declare variable.
+    //before ES6 - generally used to VAR to declare variable.
+    //after ES6 it change to LET to declare variable.
 
-let name = "Rahul";
-console.log(name);
+
 
 //rules for naming the variables
-//cannot be a reserved Keyword
+    //cannot be a reserved Keyword
+    //Should be meaningful
+    //Cannot start with a number
+    //Cannot contain a space or hyphen(-) if there are multiple words then use camelcase
+    //Are case sensitive
+
+
+let firstName = "Rahul";
+console.log(firstName);
+
+//Declare multiple variables.
+    //traditional way
+    let name1 = 'Rahul' , name2 = 'Sharma';
+    console.log(name1, name2)
+
+//Modern way
+let name3 = 'Rahul';
+let name4 = 'Sharma'
+console.log(name3, name4)
